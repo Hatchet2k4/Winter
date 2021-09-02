66632d47d33ebece190308c9903387959a333bbe
Francis Brazeau <hatchet2k4@gmail.com>
2021-09-01 23:03:01 -0400

    Started map 15 zone scripting. Started map 18.

---
+++ maps/map15.py
@@ -0,0 +1,15 @@
+import system
+import ika
+from snow import Snow
+
+def AutoExec():
+    system.engine.mapThings.append(Snow(velocity=(0, 0.5)))
+
+def to2():
+    offset_from = 16 * 16  # first vertical pos possible
+    offset_to = 7 * 16  # first vertical pos possible
+    x = system.engine.player.x - offset_from + offset_to
+    system.engine.mapSwitch('map02.ika-map', (x, 1 * 16))
+    
+def to18():    
+    system.engine.mapSwitch('map18.ika-map', (0, 0))
