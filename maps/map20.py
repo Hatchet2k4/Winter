aa3e1f4a90aa74cef03d3f23da2654e2438971f7
Francis Brazeau <hatchet2k4@gmail.com>
2021-11-21 16:16:38 -0500

    Fixed map06->map20 connection

--- maps/map20.py
+++ maps/map20.py
@@ -8,7 +8,7 @@ def to18():
     system.engine.mapSwitch('map18.ika-map', (32*16, 4*16))
     
 def to6():
-    offset_from = 7 * 16  # first vertical pos possible
+    offset_from = 5 * 16  # first vertical pos possible
     offset_to = 16 * 16  # first vertical pos possible
     x = system.engine.player.x - offset_from + offset_to
     system.engine.mapSwitch('map06.ika-map', (x, 38 * 16))
